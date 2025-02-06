fb7d7e6cba9bb3da66ce34bd84fa3fcef1909bff
francisco-gutierrez-bfg <78387434+francisco-gutierrez-bfg@users.noreply.github.com>
2025-02-06 09:22:12 -0500

    .

--- Nuevo_GCP_OlenBee/GCP/Build_GKE_Using_Terraform/Terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "kubernetes_endpoint" {
-  value = google_container_cluster.primary.endpoint
-}
-
-output "cluster_name" {
-  value = google_container_cluster.primary.name
-}
--- Nuevo_GCP_OlenBee/GCP/Build_GKE_Using_Terraform/Terraform/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-project_id     = "tu-id-de-proyecto"
-region         = "us-central1"
-cluster_name   = "mi-cluster-gke"
-node_count     = 3
-node_machine_type = "e2-medium"
